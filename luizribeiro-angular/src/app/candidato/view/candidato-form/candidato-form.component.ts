08bbe429e61006c159dcdbcee9c293789b8256e9
LuizRb07 <119629081+LuizRb07@users.noreply.github.com>
2022-12-02 13:04:56 -0300

    commit

--- luizribeiro-angular/src/app/candidato/view/candidato-form/candidato-form.component.ts
+++ luizribeiro-angular/src/app/candidato/view/candidato-form/candidato-form.component.ts
@@ -1,10 +1,12 @@
+import { Candidato } from 'src/app/model/candidato';
 import { Location } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { SuccessDialogComponent } from 'src/app/shared/components/success-dialog/success-dialog.component';
 import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
 import { CandidatoService } from '../../service/candidato.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-candidato-form',
@@ -13,45 +15,57 @@ import { CandidatoService } from '../../service/candidato.service';
 })
 export class CandidatoFormComponent {
   form = this.formBuilder.group({
+    id: [''],
     nome: ['', [Validators.minLength(4), Validators.maxLength(60)]],
     cpf: ['', [Validators.maxLength(14)]],
     rg: ['', [Validators.maxLength(12)]],
-    nascimento: [''],
+    data_nasc: [''],
     sexo: [''],
     mae: ['', [Validators.minLength(4), Validators.maxLength(60)]],
     pai: ['', [Validators.minLength(4), Validators.maxLength(60)]],
     email: [''],
     cep: [''],
     endereco: [''],
-    numero: [],
+    numero: [''],
     bairro: [''],
     cidade: [''],
     estado: [''],
     telefone_fixo: [''],
     celular: [''],
-    altura: [],
-    peso: [],
+    altura: [''],
+    peso: [''],
     tipo_sanguineo: ['']
   });
-
+  availableOptions: [
+    {id: '1', name: 'Option A'},
+    {id: '2', name: 'Option B'},
+    {id: '3', name: 'Option C'}
+  ];
   constructor (
     private formBuilder: NonNullableFormBuilder,
+    @Inject(MAT_DIALOG_DATA) public candidato: Candidato,
     private service: CandidatoService,
+    private aroute: ActivatedRoute,
     private location: Location,
     public dialog: MatDialog
   ) {
-
+    console.log(candidato);
+    this.form.setValue({
+      id: candidato.id, nome: candidato.nome, cpf: candidato.cpf, rg: candidato.rg,
+      data_nasc: candidato.data_nasc, sexo: candidato.sexo, mae: candidato.mae,
+      pai: candidato.pai, email: candidato.email, cep: candidato.cep,
+      endereco: candidato.endereco, numero: candidato.numero, bairro: candidato.bairro,
+      cidade: candidato.cidade, estado: candidato.estado,
+      telefone_fixo: candidato.telefone_fixo, celular: candidato.celular,
+      altura: candidato.altura, peso: candidato.peso, tipo_sanguineo: candidato.tipo_sanguineo
+    });
   }
 
   onSubmit() {
-    this.service.save(this.form.value).subscribe(
-      ret =>{
-        this.dialog.open(SuccessDialogComponent, {data: "Novos dados armazenados."});
-      },
-      err => {
-        this.dialog.open(ErrorDialogComponent, {data: err.message});
-      }
-    );
+    this.service.save(this.form.value).subscribe({
+      next: (v) => this.dialog.open(SuccessDialogComponent, {data: "Novos dados armazenados."}),
+      error: (e) => this.dialog.open(ErrorDialogComponent, {data: e.message})
+    });
   }
 
   onCancel() {
